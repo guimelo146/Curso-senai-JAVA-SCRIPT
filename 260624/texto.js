3df3182979e24560b56fab9e24526929ab1417db
guimelo146 <almeidaguilherme146@gmail.com>
2024-06-26 10:55:26 -0300

    ex novo typeof

---
+++ 260624/texto.js
@@ -0,0 +1,10 @@
+const nomeAluno = "Caio";
+    const casado = true;
+        const times = ["PAULISTA", "SANTOS"];
+        const idadeAlunos = 18
+
+
+console.log(typeof nomeAluno);
+  console.log(typeof idadeAlunos);
+   console.log(typeof casado);
+    console.log(typeof times);
