1472b2fc3b02100ae10cde1e198f13e4fe79858c
guimelo146 <almeidaguilherme146@gmail.com>
2024-06-26 11:29:56 -0300

    novos exercicios

---
+++ 260624/exercicio3e4.js
@@ -0,0 +1,5 @@
+//desafio 3 e 4
+   const winterMessege = "o inverno esta chegando"
+
+    console.log(winterMessege.toUpperCase());
+        console.log(winterMessege.indexOf("v"));
---
+++ 260624/texto2.js
@@ -0,0 +1,21 @@
+let txt = "estão chegando as provas"
+    
+    console.log(txt.length);
+//conta a quantidade de caracteres numa string
+
+        console.log(txt.indexOf("as"));
+//retorna a primeira posição na string
+
+    console.log(txt.lastIndexOf("as"));
+//retorna a ultima posição na string
+
+    console.log(txt.slice(0, 5));
+//determina um fim na varivel baseado na quantidade de caracteres
+
+    console.log(txt.replace("provas", "férias do fim do ano" ));
+//altera uma palavra da variavel ditada 
+
+    console.log(txt.toUpperCase());
+    console.log(txt.toLowerCase());
+//deixam a variavel ditada ou em maiusculo ou em minusculo
+
