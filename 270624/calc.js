ff72264607d5ee5802adea59de0629789f9c52cc
guimelo146 <almeidaguilherme146@gmail.com>
2024-06-27 08:17:30 -0300

    novas formas de calculos

---
+++ 270624/calc.js
@@ -0,0 +1,19 @@
+//declaração de variável
+
+    let num = 10;
+let num2 = 5;
+    
+//operações matemáticas
+
+let soma = num + num2;
+    let sub = num - num2;
+        let mult = num * num2;
+    let div = num / num2;
+
+//exibindo a variável soma
+
+console.log("o resultado da soma é: " + soma);
+    console.log("o resultado da subtração é: " + sub);
+        console.log("o resultado da multiplicação é: " + mult);
+    console.log("o resultado da divisão é: " + div);
+    
