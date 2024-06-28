c2672d6fe47831e937a1ad31c910d50743ea47ff
guimelo146 <almeidaguilherme146@gmail.com>
2024-06-28 10:48:48 -0300

    novas att para o SISTEMA ALUNO

--- SISTEMAALUNO/script.js
+++ SISTEMAALUNO/script.js
@@ -1,14 +1,27 @@
 const button = document.querySelector("button");
 const inputNota = document.querySelector("#nota");
 
+//criando uma função para limpar o campo
+function clearform() {
+    inputNota.value = "";
+}
 //criando uma função
-button.addEventListener("click", function(event){
+button.addEventListener("click", function (event) {
     event.preventDefault();
+    const inputNotavalue = inputNota.value;
+    //console.log (inputnotavalue); 
     console.log(inputNota.value);
-            
-    if(inputNota.value >=6){
+    //validação 
+    if (inputNotavalue === '') {
+        alert("Preencher a caixa de texto para validar nota")
+        return false
+    }
+    //lógica
+    if (inputNotavalue >= 6) {
         alert("Você está aprovado")
-    }else{
+    } else {
         alert("Você está reprovado")
     }
+    //chamando função limpar campo
+    clearform();
 })
